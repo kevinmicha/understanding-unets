6ee417154b8bca375c7b20a3597f6a73900dc1f7
Kevin Michalewicz <44092360+kevinmicha@users.noreply.github.com>
2021-05-03 17:38:48 +0200

    Deleted the class and stat functions part

--- learning_wavelets/evaluate/exact_recon_unet_evaluate.py
+++ learning_wavelets/evaluate/exact_recon_unet_evaluate.py
@@ -10,71 +10,7 @@ from learning_wavelets.config import LOGS_DIR, CHECKPOINTS_DIR
 from learning_wavelets.data.datasets import im_dataset_bsd68, im_dataset_bsd500
 
 from learning_wavelets.models.exact_recon_unet import ExactReconUnet
-
-
-tf.random.set_seed(1)
-
-def mse(gt, pred):
-    """ Compute Mean Squared Error (MSE) """
-    return np.mean((gt - pred) ** 2)
-
-
-def nmse(gt, pred):
-    """ Compute Normalized Mean Squared Error (NMSE) """
-    return np.linalg.norm(gt - pred) ** 2 / np.linalg.norm(gt) ** 2
-
-
-def psnr(gt, pred):
-    """ Compute Peak Signal to Noise Ratio metric (PSNR) """
-    return compare_psnr(gt, pred, data_range=gt.max() - gt.min())
-
-
-def ssim(gt, pred):
-    """ Compute Structural Similarity Index Metric (SSIM). """
-    return compare_ssim(
-        gt.transpose(1, 2, 0), pred.transpose(1, 2, 0), multichannel=True, data_range=gt.max() - gt.min()
-    )
-
-
-METRIC_FUNCS = dict(
-    LOSS=mse,
-    PSNR=psnr,
-    SSIM=ssim,
-)
-
-
-class Metrics:
-    """
-    Maintains running statistics for a given collection of metrics.
-    """
-
-    def __init__(self, metric_funcs):
-        self.metrics = {
-            metric: Statistics() for metric in metric_funcs
-        }
-        self.metric_funcs = metric_funcs
-
-    def push(self, target, recons):
-        for metric, func in self.metric_funcs.items():
-            self.metrics[metric].push(func(target, recons))
-
-    def means(self):
-        return {
-            metric: stat.mean() for metric, stat in self.metrics.items()
-        }
-
-    def stddevs(self):
-        return {
-            metric: stat.stddev() for metric, stat in self.metrics.items()
-        }
-
-    def __repr__(self):
-        means = self.means()
-        stddevs = self.stddevs()
-        metric_names = sorted(list(means))
-        return ' '.join(
-            f'{name} = {means[name]:.4g} +/- {stddevs[name]:.4g}' for name in metric_names
-        )
+from learning_wavelets.evaluate.utils import METRIC_FUNCS, Metrics
 
 def evaluate_unet(
         run_id = 'ExactReconUnet_4_bsd500_0_55_2000_1620051556-20',
